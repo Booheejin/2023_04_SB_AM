4537beb3e50d07f46ac142f4c4121ec7014c8e03
Booheejin <https://mn980456@gmail.com>
2023-04-14 14:15:33 +0900

    writeArticle

--- src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
+++ src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
@@ -18,11 +18,23 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		this.lastArticleId = 0;
 		this.articles = new ArrayList<>();
+		
+		makeTestDate();
 	}
 	
-	@RequestMapping("/usr/article/doadd")
-	@ResponseBody
-	public Article doAdd(String title,String body) {
+	private void makeTestDate() {
+		for(int i = 1; i <= 10; i++) {
+			
+			String title = "제목" + i;
+			String body = "내용" + i;		
+			
+			writeArticle(title,body);
+			
+		}
+		
+	}
+
+	private Article writeArticle(String title,String body) {
 		
 		int id = this.lastArticleId +1;
 		this.lastArticleId = id;
@@ -31,6 +43,13 @@ public class UsrArticleController {
 		articles.add(article);
 		
 		return article;
+	}
+	
+	@RequestMapping("/usr/article/doadd")
+	@ResponseBody
+	public Article doAdd(String title,String body) {
+		
+		return writeArticle(title,body);
 	}
 	
 	@RequestMapping("/usr/article/getArticles")
