1c60af593c9763d7892c7bc600e3a9de2941a158
Booheejin <https://mn980456@gmail.com>
2023-04-14 13:46:49 +0900

    ArticleController doAdd, getArticles

---
+++ src/main/java/com/koreaIT/demo/controller/UsrHomeController.java
@@ -0,0 +1,86 @@
+package com.koreaIT.demo.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Controller
+public class UsrHomeController {
+	
+	@RequestMapping("/usr/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "안녕하세요";
+	}
+	
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public Map<String,Object> showMain2() {
+		
+		Map<String,Object> map = new HashMap<>();
+		map.put("1", "asdasd");
+		map.put("2", 1234123);
+		
+		return map;
+	}
+	
+	@RequestMapping("/usr/home/main3")
+	@ResponseBody
+	public List<String> showMain3() {
+		
+		List<String> list = new ArrayList<>();
+		list.add("ads");
+		list.add("2");
+		
+		return list;
+	}
+	
+	@RequestMapping("/usr/home/main4")
+	@ResponseBody
+	public List<Map<String,Object>> showMain4() {
+		
+		List<Map<String,Object>> list = new ArrayList<>();
+		Map<String,Object> map = new HashMap<>();
+		Map<String,Object> map2 = new HashMap<>();
+		
+		map.put("키", "벨류");
+		map.put("키1", "벨류1");
+		map2.put("키", "벨류");
+		map2.put("키1", "벨류1");
+		
+		list.add(map);
+		list.add(map2);
+		
+		return list;
+	}
+	
+//	@RequestMapping("/usr/home/main5")
+//	@ResponseBody
+//	public Article showMain5() {
+//		
+////		Article article = new Article(1, "asdf");
+//		Article article2 = new Article();
+//		
+//		return article2;
+//	}
+	
+}
+
+
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//class Article {
+//	private int id;
+//	private String title;
+//	
+//}
