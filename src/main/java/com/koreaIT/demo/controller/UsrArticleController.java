5fc30eb8707bf5920972fc16c6fbff66cf9d586e
Booheejin <https://mn980456@gmail.com>
2023-04-20 13:35:37 +0900

    getArticles,doModify,doDelete에 generic 적용

--- src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
+++ src/main/java/com/koreaIT/demo/controller/UsrArticleController.java
@@ -48,40 +48,42 @@ public class UsrArticleController {
 	
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
-	public List<Article> getArticles() {
+	public ResultData<List<Article>> getArticles() {
 		
-		return articleService.getArticles();
+		return ResultData.from("S-1", "게시물 리스트",articleService.getArticles()); 
 	}
 	
 	@RequestMapping("/usr/article/dodelete")
 	@ResponseBody
-	public String doDelete(int id) {
+	public ResultData doDelete(int id) {
 		
 		Article article = articleService.getArticleById(id);
 		
 		
 		if(article == null) {
-			return id + "번 게시물은 존재하지 않습니다.";
+			return ResultData.from("F-1", Util.f("%d번 게시물은 존재하지 않습니다.", id));
 		}
 		
 		articleService.deleteArticle(id);
 		
-		return id +"번 게시물을 삭제했습니다.";
+		return ResultData.from("S-1", Util.f("%d번 게시물을 삭제했습니다.", id));
 	}
 	
 	@RequestMapping("/usr/article/domodify")
 	@ResponseBody
-	public String doModify(int id , String title , String body) {
+	public ResultData doModify(int id , String title , String body) {
 		
 		Article article = articleService.getArticleById(id);
 		
 		if(article == null) {
-			return id + "번 게시물은 존재하지 않습니다.";
+			return ResultData.from("F-1", Util.f("%d번 게시물은 존재하지 않습니다.", id));
+			
 		}
 		
 		articleService.modifyArticle(id,title,body);
 		
-		return id +"번 게시물을 수정했습니다.";
+		return ResultData.from("S-1", Util.f("%d번 게시물을 수정했습니다.", id));
+		
 	}
 	
 	@RequestMapping("/usr/article/getArticle")
