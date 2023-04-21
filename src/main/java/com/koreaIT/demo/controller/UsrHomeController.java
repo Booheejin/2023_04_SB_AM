85f4a66cb0d73909ca8dc21e623292f7859b91b1
Booheejin <https://mn980456@gmail.com>
2023-04-21 14:56:35 +0900

    jsp connect,main page

--- src/main/java/com/koreaIT/demo/controller/UsrHomeController.java
+++ src/main/java/com/koreaIT/demo/controller/UsrHomeController.java
@@ -17,51 +17,55 @@ import lombok.NoArgsConstructor;
 public class UsrHomeController {
 	
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
 	public String showMain() {
-		return "안녕하세요";
+		return "usr/home/main";
 	}
 	
-	@RequestMapping("/usr/home/main2")
-	@ResponseBody
-	public Map<String,Object> showMain2() {
-		
-		Map<String,Object> map = new HashMap<>();
-		map.put("1", "asdasd");
-		map.put("2", 1234123);
-		
-		return map;
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
 	}
 	
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public List<String> showMain3() {
-		
-		List<String> list = new ArrayList<>();
-		list.add("ads");
-		list.add("2");
-		
-		return list;
-	}
-	
-	@RequestMapping("/usr/home/main4")
-	@ResponseBody
-	public List<Map<String,Object>> showMain4() {
-		
-		List<Map<String,Object>> list = new ArrayList<>();
-		Map<String,Object> map = new HashMap<>();
-		Map<String,Object> map2 = new HashMap<>();
-		
-		map.put("키", "벨류");
-		map.put("키1", "벨류1");
-		map2.put("키", "벨류");
-		map2.put("키1", "벨류1");
-		
-		list.add(map);
-		list.add(map2);
-		
-		return list;
-	}
+//	@RequestMapping("/usr/home/main2")
+//	@ResponseBody
+//	public Map<String,Object> showMain2() {
+//		
+//		Map<String,Object> map = new HashMap<>();
+//		map.put("1", "asdasd");
+//		map.put("2", 1234123);
+//		
+//		return map;
+//	}
+//	
+//	@RequestMapping("/usr/home/main3")
+//	@ResponseBody
+//	public List<String> showMain3() {
+//		
+//		List<String> list = new ArrayList<>();
+//		list.add("ads");
+//		list.add("2");
+//		
+//		return list;
+//	}
+//	
+//	@RequestMapping("/usr/home/main4")
+//	@ResponseBody
+//	public List<Map<String,Object>> showMain4() {
+//		
+//		List<Map<String,Object>> list = new ArrayList<>();
+//		Map<String,Object> map = new HashMap<>();
+//		Map<String,Object> map2 = new HashMap<>();
+//		
+//		map.put("키", "벨류");
+//		map.put("키1", "벨류1");
+//		map2.put("키", "벨류");
+//		map2.put("키1", "벨류1");
+//		
+//		list.add(map);
+//		list.add(map2);
+//		
+//		return list;
+//	}
 	
 //	@RequestMapping("/usr/home/main5")
 //	@ResponseBody
